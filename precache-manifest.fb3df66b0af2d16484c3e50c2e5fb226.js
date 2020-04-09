ab69cf64e51b44bb46c00c9e93395574be0c7561
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-10 02:21:46 +0530

    Updates

---
+++ precache-manifest.fb3df66b0af2d16484c3e50c2e5fb226.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e36b19de110ad3924c9574ca20a6d5",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "985c3111fc32a5b23a28",
+    "url": "/weather/static/css/main.544b497d.chunk.css"
+  },
+  {
+    "revision": "b45d26519469e555c633",
+    "url": "/weather/static/js/2.30a8c5bc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.30a8c5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985c3111fc32a5b23a28",
+    "url": "/weather/static/js/main.40517d18.chunk.js"
+  },
+  {
+    "revision": "5155611f3a8ef137db4c",
+    "url": "/weather/static/js/runtime-main.e4f5fc01.js"
+  },
+  {
+    "revision": "5c2d90ea427f1837d732eaf17c42e514",
+    "url": "/weather/static/media/app1.5c2d90ea.jpg"
+  },
+  {
+    "revision": "33e6360eacb45c0f97a01d0f03b23886",
+    "url": "/weather/static/media/sunny.33e6360e.jpg"
+  }
+]);
